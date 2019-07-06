713145a345d6f21c9e7739cf88de38de5566fa97
Kyle Trusler <ktrusler@arbormetrix.com>
2019-07-06 23:22:39 +0200

    Add Cesium

--- now.sh
+++ now.sh
@@ -12,7 +12,8 @@
       "env": {
         "NODE_ENV": "DEVELOPMENT",
         "HASURA_GRAPHQL_URL": "@hasura-url-development",
-        "HASURA_GRAPHQL_ADMIN_SECRET": "@hasura-secret"
+        "HASURA_GRAPHQL_ADMIN_SECRET": "@hasura-secret",
+        "CESIUM_KEY": "@cesium-key"
       }
   }
 }
